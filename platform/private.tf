c93eb35fe41561dbd4c5cc6d7bad5762261e774b
Christoffer Claesson <Christoffer.Claesson@securitybits.io>
2023-06-02 22:18:35 +0200

    removed plex terraform

--- platform/private.tf
+++ platform/private.tf
@@ -43,65 +43,6 @@ resource "proxmox_vm_qemu" "private-docker-01" {
     }
 }
 
-# resource "proxmox_vm_qemu" "private-plex" {
-    
-#     # VM General Settings
-#     target_node = "pve-node-01"
-#     name = "private-plex"
-#     desc = "Created with Terraform"
-
-#     # VM Advanced General Settings
-#     onboot = true 
-
-#     # VM OS Settings
-#     clone = "Ubuntu-22.04-Template-250GB"
-
-#     # VM System Settings
-#     agent = 1
-    
-#     # VM CPU Settings
-#     cores = 2
-#     sockets = 1
-#     cpu = "host"
-    
-#     # VM Memory Settings
-#     memory = 2048
-    
-#     network {
-#         macaddr = "00:50:56:b9:ef:61"
-#         bridge = "vmbr0"
-#         model  = "virtio"
-#         tag = 160
-#     }
-
-#     # Set the disk size corresponding to the Template size
-#     disk {
-#         storage = "vm"
-#         type = "scsi"
-#         size = "250G"
-#     }
-
-#     # VM Cloud-Init Settings
-#     os_type = "cloud-init"
-
-#     #os_type = "cloud-init"
-#     connection {
-#       type      = "ssh"
-#       user      = var.SSH_USER
-#       password  = var.SSH_PASS
-#       host      = self.ssh_host
-#       script_path = "/home/${var.SSH_USER}/provision_%RAND%.sh"
-#     }
-
-#     provisioner "remote-exec" {
-#       inline = [
-#           "sleep 10",
-#           "sudo hostnamectl set-hostname ${self.name}",
-#           "sudo reboot"
-#         ]
-#     }
-# }
-
 resource "proxmox_vm_qemu" "private-ytdl" {
     
     # VM General Settings
