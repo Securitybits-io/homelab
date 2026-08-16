77dcdae3fac410b39bd67e71601e5b601d91dbfb
Christoffer Claesson <Christoffer.Claesson@securitybits.io>
2026-08-16 22:26:41 +0200

    updated template tf

--- templates/debian-13.tf
+++ templates/debian-13.tf
@@ -1,28 +1,47 @@
+locals {
+  debian_13_image = {
+    url       = "https://cloud.debian.org/images/cloud/trixie/latest/debian-13-genericcloud-amd64.qcow2"
+    file_name = "debian-13-trixie-genericcloud-amd64.qcow2"
+  }
+
+  debian_13_template = {
+    vm_id                = 9000
+    name                 = "debian-13-trixie-template"
+    description          = "Base Debian 13 trixie cloud-init template for Terraform"
+    cpu_cores            = 1
+    memory_mb            = 512
+    disk_size            = 30
+    network_bridge       = var.VM_BRIDGE
+    cloud_init_datastore = var.STORAGE_POOL
+    cloud_init_ipv4      = "dhcp"
+  }
+}
+
 resource "proxmox_virtual_environment_file" "debian_13_cloud_image" {
   content_type = "import"
   datastore_id = var.FILE_STORAGE
   node_name    = var.PVE_NODE
 
   source_file {
-    path      = "https://cloud.debian.org/images/cloud/trixie/latest/debian-13-genericcloud-amd64.qcow2"
-    file_name = "debian-13-trixie-genericcloud-amd64.qcow2"
+    path      = local.debian_13_image.url
+    file_name = local.debian_13_image.file_name
   }
 }
 
 resource "proxmox_virtual_environment_vm" "debian_13_template" {
   node_name   = var.PVE_NODE
-  vm_id       = 9000
-  name        = "debian-13-trixie-template"
-  description = "Base Debian 13 trixie cloud-init template for Terraform"
+  vm_id       = local.debian_13_template.vm_id
+  name        = local.debian_13_template.name
+  description = local.debian_13_template.description
   template    = true
   started     = false
 
   cpu {
-    cores = 1
+    cores = local.debian_13_template.cpu_cores
   }
 
   memory {
-    dedicated = 512
+    dedicated = local.debian_13_template.memory_mb
   }
 
   agent {
@@ -32,16 +51,16 @@ resource "proxmox_virtual_environment_vm" "debian_13_template" {
   disk {
     datastore_id = var.STORAGE_POOL
     interface    = "scsi0"
-    size         = 30
+    size         = local.debian_13_template.disk_size
     file_id      = proxmox_virtual_environment_file.debian_13_cloud_image.id
   }
 
   initialization {
-    datastore_id = var.STORAGE_POOL
+    datastore_id = local.debian_13_template.cloud_init_datastore
 
     ip_config {
       ipv4 {
-        address = "dhcp"
+        address = local.debian_13_template.cloud_init_ipv4
       }
     }
 
@@ -49,7 +68,7 @@ resource "proxmox_virtual_environment_vm" "debian_13_template" {
   }
 
   network_device {
-    bridge = var.VM_BRIDGE
+    bridge = local.debian_13_template.network_bridge
     model  = "virtio"
   }
 
