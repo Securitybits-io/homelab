26170428bcaad3bb8cba5f69d836722ebd2dd5d4
Christoffer Claesson <Christoffer.Claesson@securitybits.io>
2026-08-18 13:38:00 +0200

    updated template to include netplan

--- templates/cloud-init.tf
+++ templates/cloud-init.tf
@@ -9,6 +9,7 @@ resource "proxmox_virtual_environment_file" "debian_13_template_cloud_init" {
     data = templatefile("${path.module}/templates/ansible-bootstrap.cloud-init.tftpl", {
       ansible_user           = var.ANSIBLE_USER
       ansible_ssh_public_key = var.ANSIBLE_SSH_PUBLIC_KEY
+      netplan_interface      = var.NETPLAN_INTERFACE
     })
   }
 }
